1d304718d13e28a6d99afbbcfa162449cb1d719f
Rajesh <rajesh@Rajeshs-Mac.local>
2019-01-10 01:38:38 -0800

    product insert

---
+++ Coder-swag/Coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  Coder-swag
+//
+//  Created by Rajesh on 10/01/2019.
+//  Copyright © 2019 Rajesh. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
